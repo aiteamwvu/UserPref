510adb429eda1012658d8a613c7d0a131d813512
Salvi <salvisingh.yav@gmail.com>
2017-12-02 05:12:07 -0500

    Initial commit

---
+++ printy.py
@@ -0,0 +1,5 @@
+import importingdata.py
+
+print (all_documents[1])
+print (all_documents[45])
+print (all_documents[67])
--- userpreference.py
+++ userpreference.py
@@ -1,28 +1,100 @@
 import pandas as pd
 import numpy as np
+import json
+from newspaper import Article
+from rake_nltk import Rake
 import tensorflow as tf
 from collections import Counter
 from sklearn.datasets import fetch_20newsgroups
 
-categories = ["comp.graphics","sci.space","rec.sport.baseball"]
-newsgroups_train = fetch_20newsgroups(subset='train', categories=categories)
-newsgroups_test = fetch_20newsgroups(subset='test', categories=categories)
 
-print('total texts in train:',len(newsgroups_train.data))
-print('total texts in test:',len(newsgroups_test.data))
 
-print('text',newsgroups_train.data[0])
-print('category:',newsgroups_train.target[0])
+
+file = open("example_1.json").read()
+arr = json.loads(file)
+count = 0
+all_documents = []
+for article in arr:
+    if "content" in article and article['source_content'] != 'video' and article['validated'] == -2:
+
+        url = article['_id']
+        art = Article(url, language='en')  # English
+        try:
+            art.download()
+            art.parse()
+            art_content =  art.text
+            #print(article['_id'])
+            all_documents.append(art_content)
+            count = count + 1
+            #print(count)
+            if(count == 93):
+                break
+        except:
+            print('bad article')
+            print(article['source_content'])
+            print(article['_id'])
+            continue
+
+print(type(all_documents))
+
+test_d =  pd.read_csv("/Users/eowyna/Documents/GitHub/UserPref/Test.tsv", sep='\t', encoding='latin-1')
+
+test_data = test_d.iloc[:, 0].tolist()
+
+print(type(test_data))
+
+
+
+#categories = ["comp.graphics","sci.space","rec.sport.baseball"]
+#newsgroups_train = fetch_20newsgroups(subset='train', categories=categories)
+#newsgroups_test = fetch_20newsgroups(subset='test', categories=categories)
+
+#print('total texts in train:',len(newsgroups_train.data))
+#print('total texts in test:',len(newsgroups_test.data))
+
+#print('text',newsgroups_train.data[0])
+#print('category:',newsgroups_train.target[0])
+
+list_labels = [0] * 93
+testlist_labels = [0]*20
+list_labels[2] = 1
+list_labels[6] = 1
+list_labels[12] = 1
+list_labels[14] = 1
+list_labels[38] = 1
+list_labels[42] = 1
+list_labels[47] = 1
+list_labels[49] = 1
+list_labels[50] = 1
+list_labels[52] = 1
+list_labels[54] = 1
+list_labels[56] = 1
+list_labels[60] = 1
+list_labels[61] = 1
+list_labels[65] = 1
+list_labels[68] = 1
+list_labels[73] = 1
+list_labels[78] = 1
+list_labels[85] = 1
+list_labels[90] = 1
+testlist_labels[0] = 1
+testlist_labels[1] = 1
+testlist_labels[2] = 1
+testlist_labels[3] = 1
+testlist_labels[4] = 1
+
 
 
 vocab = Counter()
 
-for text in newsgroups_train.data:
-    for word in text.split(' '):
+for text in range(len(all_documents)):
+    content = all_documents[text]
+    for word in content.split(' '):
         vocab[word.lower()]+=1
 
-for text in newsgroups_test.data:
-    for word in text.split(' '):
+for text in range(len(test_data)):
+    content1 = test_data[text]
+    for word in content1.split(' '):
         vocab[word.lower()]+=1
 
 
@@ -40,50 +112,45 @@ def get_word_2_index(vocab):
 
 word2index = get_word_2_index(vocab)
 
-print("Index of the word 'the':",word2index['the'])
+
+print("Index of the word 'is':",word2index['is'])
 
 
-def get_batch(df,i,batch_size):
+def get_batch(data,label,i,batch_size):
     batches = []
     results = []
-    texts = df.data[i*batch_size:i*batch_size+batch_size]
-    categories = df.target[i*batch_size:i*batch_size+batch_size]
-    for text in texts:
+    texts = data[i*batch_size:i*batch_size+batch_size]
+    categories = label[i*batch_size:i*batch_size+batch_size]
+
+    for i in range(len(texts)):
+
+        text = texts[i]
         layer = np.zeros(total_words,dtype=float)
         for word in text.split(' '):
             layer[word2index[word.lower()]] += 1
-
         batches.append(layer)
 
     for category in categories:
-        y = np.zeros((3),dtype=float)
+        y = np.zeros((2),dtype=float)
         if category == 0:
             y[0] = 1.
-        elif category == 1:
-            y[1] = 1.
         else:
-            y[2] = 1.
+            y[1] = 1.
         results.append(y)
 
 
     return np.array(batches),np.array(results)
 
-print("Each batch has 100 texts and each matrix has 119930 elements (words):",get_batch(newsgroups_train,1,100)[0].shape)
 
-print("Each batch has 100 labels and each matrix has 3 elements (3 categories):",get_batch(newsgroups_train,1,100)[1].shape)
-
-
-# Parameters
 learning_rate = 0.01
 training_epochs = 10
-batch_size = 150
+batch_size = 15
 display_step = 1
 
-# Network Parameters
 n_hidden_1 = 100      # 1st layer number of features
 n_hidden_2 = 100       # 2nd layer number of features
 n_input = total_words # Words in vocab
-n_classes = 3         # Categories: graphics, sci.space and baseball
+n_classes = 2
 
 input_tensor = tf.placeholder(tf.float32,[None, n_input],name="input")
 output_tensor = tf.placeholder(tf.float32,[None, n_classes],name="output")
@@ -137,10 +204,12 @@ with tf.Session() as sess:
     # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
-        total_batch = int(len(newsgroups_train.data)/batch_size)
+        total_batch = int(len(all_documents)/batch_size)
+
         # Loop over all batches
         for i in range(total_batch):
-            batch_x,batch_y = get_batch(newsgroups_train,i,batch_size)
+
+            batch_x,batch_y = get_batch(all_documents,list_labels,i,batch_size)
             # Run optimization op (backprop) and cost op (to get loss value)
             c,_ = sess.run([loss,optimizer], feed_dict={input_tensor: batch_x,output_tensor:batch_y})
             # Compute average loss
@@ -156,6 +225,6 @@ with tf.Session() as sess:
     correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(output_tensor, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    total_test_data = len(newsgroups_test.target)
-    batch_x_test,batch_y_test = get_batch(newsgroups_test,0,total_test_data)
+    total_test_data = len(test_data)
+    batch_x_test,batch_y_test = get_batch(test_data, testlist_labels,0,total_test_data)
     print("Accuracy:", accuracy.eval({input_tensor: batch_x_test, output_tensor: batch_y_test}))
